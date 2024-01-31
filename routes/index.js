8676473f8a1f48d720b32c810c1cc41a369126b7
Alex D. <t.docheff@gmail.com>
2024-01-30 21:22:36 -0300

    Removed all files that are in the .gitignore

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const route = express.Router();
-
-const userRoutes = require('../routes/userRoutes');
-
-route.use('/usuarios', userRoutes);
-
-module.exports = route;
